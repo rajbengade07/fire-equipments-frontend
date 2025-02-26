e0ed587f6d477f742b07068c88ee40a19b834165
Raj Ramesh Bengade <157134976+rajbengade07@users.noreply.github.com>
2025-02-26 21:57:28 +0530

    Add files via upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,26 @@
+import React, { useState } from 'react';
+import './Header.css';
 
+const Header = () => {
+    const [menu, setMenu] = useState("home");
+
+    const handleViewMenu = () => {
+        setMenu("menu");
+        const menuSection = document.getElementById('explore-menu');
+        if (menuSection) {
+            menuSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Your Safety, Our Priority</h2>
+                <p ><strong>Ensure Safety with Reliable Fire Equipment Services! From installation to maintenance, we provide top-quality fire safety solutions to protect your home and business. Stay secure with our expert services!</strong></p>
+                <button type="button" onClick={handleViewMenu}>View Menu</button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
