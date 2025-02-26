f18cbf7c2130a5c37b1035c6890e479e0df1cf96
Raj Ramesh Bengade <157134976+rajbengade07@users.noreply.github.com>
2025-02-26 21:56:54 +0530

    Add files via upload

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,1 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
 
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            {/* <img src={assets.logo} alt="" /> */}
+            <p>"We’d love to hear from you! Whether you have questions, feedback, or need assistance, our team is here to help. Reach out to us anytime, and we’ll get back to you as soon as possible. Your satisfaction is our priority, and we’re committed to making your experience exceptional. Let’s connect!"</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+1-212-456-7890</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
