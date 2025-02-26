fc0b9ccd9fdb2b2f8c8678e0d36b04f3c1686020
Raj Ramesh Bengade <157134976+rajbengade07@users.noreply.github.com>
2025-02-26 20:43:42 +0530

    Add files via upload

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,1 +1,23 @@
+import React, { useState } from 'react'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+// import AppDownload from '../../components/AppDownload/AppDownload'
+import EnquiryForm from '../../components/EnquiryForm/EnquiryForm'
 
+const Home = () => {
+
+  const [category,setCategory] = useState("All")
+
+  return (
+    <>
+      <Header/>
+      <ExploreMenu setCategory={setCategory} category={category}/>
+      <FoodDisplay category={category}/>
+      {/* <AppDownload/> */}
+      <EnquiryForm/>
+    </>
+  )
+}
+
+export default Home
